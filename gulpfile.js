ee8f5ea2ed1f3268b17371e849f34eea916d612a
kamijin_fanta <kamijin@live.jp>
2017-07-19 13:27:14 +0900

    JSONの更新でもLiveReloadがちゃんと効くようにした

--- gulpfile.js
+++ gulpfile.js
@@ -8,39 +8,15 @@ var browserSync =require('browser-sync');
 var del = require('del');
 var runSequence = require('run-sequence');
 
-var timetable = require("./app/_data/timetable.json");
-var handson = require("./app/_data/handons.json");
-var platinumSponsors = require("./app/_data/platinum-sponsors.json");
-var goldSponsors = require("./app/_data/gold-sponsors.json");
-var silverSponsors = require("./app/_data/silver-sponsors.json");
-var bronzeSponsors = require("./app/_data/bronze-sponsors.json");
-var jobs = require("./app/_data/jobs.json");
-var scalaStaff = require("./app/_data/scala-staff.json");
-var networkStaff = require("./app/_data/network-staff.json");
-var studentStaff = require("./app/_data/student-staff.json");
+var siteData = require("./app/_data/");
 
 gulp.task("ejs", function() {
   gulp.src("./app/index.ejs")
-  	.pipe(ejs({
-      timetable: timetable,
-      handson: handson,
-      sponsors: {
-        platinum: platinumSponsors,
-        gold: goldSponsors,
-        silver: silverSponsors,
-        bronze: bronzeSponsors
-      },
-      staff: {
-        scala: scalaStaff,
-        network: networkStaff,
-        student: studentStaff
-      },
-      jobs: jobs
-    }, {"ext": ".html"}))
+  	.pipe(ejs(siteData.getAllData(), {"ext": ".html"}))
   	.pipe(gulp.dest("./dist"));
 
   gulp.src("./app/jobs.ejs")
-  	.pipe(ejs({ jobs: jobs }, {"ext": ".html"}))
+  	.pipe(ejs({ jobs: siteData.getAllData().jobs }, {"ext": ".html"}))
   	.pipe(gulp.dest("./dist"));
 });
 
